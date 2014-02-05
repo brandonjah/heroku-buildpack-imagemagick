00b5380ecacd5128c69ec65bf2d70e2ce02d6aac
brandonjah <brandon.jahner@gmail.com>
2014-02-04 21:19:22 -0800

    Update configs.sh

--- configs.sh
+++ configs.sh
@@ -1,5 +1,5 @@
 export LIBPNG_VERSION="1.6.8"
-export IMAGE_MAGICK_VERSION="6.8.6-3"
+export IMAGE_MAGICK_VERSION="6.8.6-9"
 export REMOTE_PATH="imagemagick-${IMAGE_MAGICK_VERSION}.tgz"
 export LIBPNG_REMOTE_PATH="libpng-${LIBPNG_VERSION}.tgz"
 export S3_BUCKET="buildpack-imagemagick"
