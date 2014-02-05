f1258c4542c4c2c312d0912736efec64bd0472e3
brandonjah <brandon.jahner@gmail.com>
2014-02-05 09:49:05 -0800

    Update configs.sh

--- configs.sh
+++ configs.sh
@@ -1,4 +1,4 @@
-export LIBPNG_VERSION="1.6.8"
+export LIBPNG_VERSION="1.5.14"
 export IMAGE_MAGICK_VERSION="6.8.6-10"
 export REMOTE_PATH="imagemagick-${IMAGE_MAGICK_VERSION}.tgz"
 export LIBPNG_REMOTE_PATH="libpng-${LIBPNG_VERSION}.tgz"
